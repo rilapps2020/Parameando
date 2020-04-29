782d7a460da4498f2f3d4a885d04c0157cae9a15
Juan Pablo Díaz Correa <40843763+Juanipis@users.noreply.github.com>
2020-04-28 23:08:14 -0500

    Character cardboar VR
    En este commit trabaje en el personaje VR cardboard, el modelo rota conforme la camara rota pero al mirar hacia atras o un lado el personaje no se queda detras de la camara

---
+++ Assets/Scripts/Movement/RotatePlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlayer : MonoBehaviour {
+
+	public Camera camara;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(camara.transform.position.x, camara.transform.position.y - 1.669f, camara.transform.position.z - 0.3f); //Fixed position
+		transform.localRotation = Quaternion.Euler(0, camara.transform.eulerAngles.y, 0); //Fixed Angle
+	}
+}
