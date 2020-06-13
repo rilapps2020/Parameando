9db202c79115e307a6575f6166127bed4c6915bb
Juan Pablo Díaz Correa <40843763+Juanipis@users.noreply.github.com>
2020-06-13 15:56:12 -0500

    Menu de primera vez
    En esta versión se hizo el menu de primera vez donde la persona elige como quiere entrar por primera vez, luego de esta elección queda guardado en en PlayerPrefs.
    La variable state es la encargada de esto, si es igual a 0 es la primera vez, si es igual a 1 se cargara por defecto el menu en modo normal, si es igual a dos se cargara por defecto el modo VR

---
+++ Assets/Scripts/Menu/Change_scene.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Change_scene : MonoBehaviour {
+
+	void Start()
+	{
+		int state = PlayerPrefs.GetInt("State");
+		
+		if (state == 0)
+		{
+			Debug.Log("Primera vez");
+		}
+
+		if (state == 1)
+		{
+			Debug.Log("Modo normal");
+		}
+
+		if (state == 2)
+		{
+			Debug.Log("Modo VR");
+		}
+	}
+	public void ChangeSceneTo(string sceneName) ///State 0 = Primera vez, State 1 = Modo normal, State 2 = Modo VR
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+
+	public void SetScene(int usrState)
+	{
+		PlayerPrefs.SetInt("State", usrState);
+	}
+}
