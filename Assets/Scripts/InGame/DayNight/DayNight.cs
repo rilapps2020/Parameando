29f8bd3949ea01aadcda860d6a1667ea41ceaa92
Juan Pablo Díaz Correa <40843763+Juanipis@users.noreply.github.com>
2020-06-30 15:28:58 -0500

    Añadido ciclo día y noche
    Se añadio el efecto para simular el dia y la noche, el nombre del archivo es DayNight.cs.
    Autor original @ddragnell
    
    Co-Authored-By: ddragnell <ddragnell@users.noreply.github.com>

---
+++ Assets/Scripts/InGame/DayNight/DayNight.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNight : MonoBehaviour
+{
+
+	//Sliders
+	private float slider;
+	private float slider2;
+	private float Hour;
+	private float Tod;
+
+	//Luces
+	public Light sun;
+
+	//Velocidad de cambio
+	public int speed;
+
+	//SkyBox
+	public Material NightSkyBoxMaterial;
+	public Material DaySkyBoxMaterial;
+
+	// ------Colores del cielo ------//
+
+	//Niebla
+	public Color NightFogColor;
+	public Color DuskFogColor;
+	public Color MorningFogColor;
+	public Color MiddayFogColor;
+
+	//Ambiente
+	public Color NightAmbientLight;
+	public Color DuskAmbientLight;
+	public Color MorningAmbientLight;
+	public Color MiddayAmbientLight;
+
+	//Tinte
+	public Color NightTint;
+	public Color DuskTint;
+	public Color MorningTint;
+	public Color MiddayTint;
+
+	//Colores del sol
+	public Color SunNight;
+	public Color SunDay;
+
+	// ------Colores del cielo ------//
+
+	//Agua
+	public GameObject Water;
+	public Boolean IncludeWater;
+	public Color WaterNight;
+	public Color WaterDay;
+
+
+
+	// Inicio del ciclo, se empieza de noche
+	void Update()
+	{
+		if (slider >= 1.0f)
+		{
+			slider = 0;
+		}
+		Hour = slider * 24;
+		Tod = slider * 24;
+		sun.transform.localEulerAngles = new Vector3((slider * 360) - 90, 0, 0);
+		slider = slider + Time.deltaTime / speed;
+		sun.color = Color.Lerp(SunNight, SunDay, slider * 2);
+
+		if (IncludeWater == true)
+		{
+			Water.GetComponent<Renderer>().material.SetColor(1, Color.blue);
+		}
+
+		if (slider < 0.5f)
+		{
+			slider2 = slider;
+		}
+		else if (slider > 0.5f)
+		{
+			slider2 = 1 - slider;
+		}
+
+		if (Tod < 4)
+		{
+			//Noche
+			RenderSettings.skybox = NightSkyBoxMaterial;
+			RenderSettings.skybox.SetFloat("_Blend", 0);
+			RenderSettings.skybox.SetFloat("_Blend", (Tod / 2) - 2);
+
+			NightSkyBoxMaterial.SetColor("_Tint", Color.Lerp(NightTint, DuskTint, (Tod / 2) - 2));
+			RenderSettings.ambientLight = Color.Lerp(NightAmbientLight, DuskAmbientLight, (Tod / 2) - 2);
+			RenderSettings.fogColor = Color.Lerp(NightFogColor, DuskFogColor, (Tod / 2) - 2);
+			//En la oscuridad
+		}
+
+		if (Tod > 6 && Tod < 8)
+		{
+			RenderSettings.skybox = DaySkyBoxMaterial;
+			RenderSettings.skybox.SetFloat("_Blend", 0);
+			RenderSettings.skybox.SetFloat("_Blend", (Tod / 2) - 3);
+			DaySkyBoxMaterial.SetColor("_Tint", Color.Lerp(DuskTint, MorningTint, (Tod / 2) - 3));
+			RenderSettings.ambientLight = Color.Lerp(DuskAmbientLight, MorningAmbientLight, (Tod / 2) - 3);
+			RenderSettings.fogColor = Color.Lerp(DuskFogColor, MorningFogColor, (Tod / 2) - 3);
+			//En la mañana
+
+		}
+
+		if (Tod > 8 && Tod < 10)
+		{
+			RenderSettings.ambientLight = MiddayAmbientLight;
+			RenderSettings.skybox = DaySkyBoxMaterial;
+			RenderSettings.skybox.SetFloat("_Blend", 1);
+			DaySkyBoxMaterial.SetColor("_Tint", Color.Lerp(MorningTint, MiddayTint, (Tod / 2) - 4));
+			RenderSettings.ambientLight = Color.Lerp(MorningAmbientLight, MiddayAmbientLight, (Tod / 2) - 4);
+			RenderSettings.fogColor = Color.Lerp(MorningFogColor, MiddayFogColor, (Tod / 2) - 4);
+
+			//Al mediodia
+
+		}
+	}
+}
